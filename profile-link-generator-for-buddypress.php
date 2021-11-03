5e5f024c29fabc840498eda51f4a444369408af4
cromacio07 <93533452+cromacio07@users.noreply.github.com>
2021-11-03 09:39:02 +0530

    Add files via upload

---
+++ profile-link-generator-for-buddypress.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Plugin Name: Profile Link Generator for Buddypress
+* Description: Redirects Your Users to Their Buddypress Profile Page. Just Create A Blank Page With Permalink Base As Profile. Rest All Will Be Taken Care By Us. Enjoy..!!
+* Version: 1.0.3
+* Author: Yaglewad Onkar
+* Author URI: https://onkar-yaglewad.pantheonsite.io
+* License:      GPL2
+* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+**/
+
+function onkar_redirect_2_profile(){
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if($_SERVER['REQUEST_URI'] == '/profile/' && is_plugin_active('buddypress/bp-loader.php') && is_user_logged_in()){
+		global $current_user;
+		wp_redirect( get_bloginfo('url') . '/members/'. $current_user->user_login . '/profile/'); 
+		exit();
+	}
+		elseif($_SERVER['REQUEST_URI'] == '/profile/' && is_plugin_active('buddypress/bp-loader.php') ){
+		global $current_user;
+		wp_redirect( get_bloginfo('url') . '/wp-login.php/' ); 
+		exit(); }
+ }
+add_action('init', 'onkar_redirect_2_profile');
