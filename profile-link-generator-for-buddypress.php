fa2a88f848f280f85be4ac53d99747b97f348015
cromacio07 <93533452+cromacio07@users.noreply.github.com>
2021-11-13 09:36:17 +0530

    Update profile-link-generator-for-buddypress.php

--- profile-link-generator-for-buddypress.php
+++ profile-link-generator-for-buddypress.php
@@ -2,14 +2,14 @@
 /**
 * Plugin Name: Profile Link Generator for Buddypress
 * Description: Redirects Your Users to Their Buddypress Profile Page. Just Create A Blank Page With Permalink Base As Profile. Rest All Will Be Taken Care By Us. Enjoy..!!
-* Version: 1.0.3
-* Author: Yaglewad Onkar
-* Author URI: https://onkar-yaglewad.pantheonsite.io
-* License:      GPL2
-* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+* Version: 1.0.4
+* Author: Rubix from SPARCK
+* Author URI: https://drip-sparck.pantheonsite.io
+* License:      GPL3
+* License URI:  https://www.gnu.org/licenses/gpl-3.0.html
 **/
 
-function profile_link_generator_62(){
+function profile_link_generator_by_rubix(){
 	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 	if($_SERVER['REQUEST_URI'] == '/profile/' && is_plugin_active('buddypress/bp-loader.php') && is_user_logged_in()){
 		global $current_user;
@@ -17,8 +17,7 @@ function profile_link_generator_62(){
 		exit();
 	}
 		elseif($_SERVER['REQUEST_URI'] == '/profile/' && is_plugin_active('buddypress/bp-loader.php') ){
-		global $current_user;
 		wp_redirect( get_bloginfo('url') . '/wp-login.php/' ); 
 		exit(); }
  }
-add_action('init', 'profile_link_generator_62');
+add_action('init', 'profile_link_generator_by_rubix');
